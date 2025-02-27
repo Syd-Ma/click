a3823732e133a6264b8d8e3a2b1f94ac1836aa68
Syd-Ma <fridder06@gmail.com>
2025-02-27 17:58:55 +0200

    addIRandomCoordinates

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import logo from './public/logo192.png';
 
 export default function App() {
 
-    const [items, setItems] = React.useState<{ id: number; image: string }[]>([]);
+    const [items, setItems] = React.useState<{ id: number; image: string; x: number; y: number }[]>([]);
     const [balance, setBalance] = React.useState(0);
 
     const addItem = () => {
-        const item = { id: Date.now(), image: '/logo192.png' };
+        const randomX = Math.floor(Math.random() * window.innerWidth * 0.8);
+        const randomY = Math.floor(Math.random() * window.innerHeight * 0.8);
+        const item = { id: Date.now(), image: '/logo192.png', x: randomX, y: randomY };
         setBalance(prevBalance => prevBalance + 1);
         setItems([...items, item]);
     }
@@ -28,7 +30,13 @@ export default function App() {
             <div className={"elements"}>
                 {items.map((item) => (
                     <div className={"element"} key={item.id}>
-                        <img src={item.image} alt="Item"/>
+                        <img src={item.image} alt="Item"
+                             style={{
+                                 position: 'absolute',
+                                 left: `${item.x}px`,
+                                 top: `${item.y}px`
+                             }}
+                        />
                     </div>
                 ))}
             </div>
