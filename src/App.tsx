741ea528802cf427fbfb4b3072c486077b7b6189
Syd-Ma <fridder06@gmail.com>
2025-02-27 16:46:57 +0200

    клір

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,33 @@ import './App.css';
 export default function App() {
 
     const [items, setItems] = React.useState<{ id: number; text: string}[]>([]);
+    const [balance, setBalance] = React.useState(0);
 
     const addItem = () => {
         const item = {id: Date.now(), text: "huy"};
+        setBalance(prevBalance => prevBalance + 1);
         setItems([...items, item]);
     }
-
+    const clearItems = () => {
+        setBalance(0);
+        setItems([]);
+    };
   return (
     <div className="App">
+        <div className={"balance"}>
+            <h1 className={"balik"}>Your balance {balance}</h1>
+            <button className={"clear"} onClick={clearItems}>clear</button>
+        </div>
+
       <div className={"clicker"}>
         <button className={"bablo"} onClick={addItem}>bablo</button>
-          <ul>
+          <div className={"elements"}>
               {items.map((item) => (
-                  <li key={item.id}>{item.text}</li>
+                  <div className={"element"} key={item.id}>{item.text}</div>
               ))}
-          </ul>
+          </div>
       </div>
-    </div>
+      </div>
   );
 }
 
